863b6e3dcf8b86270a0af0396ffbafaa538d8e44
ClassicalJo <jb.barrientos@yahoo.com>
2019-11-26 20:50:34 +1100

    Updates

---
+++ precache-manifest.26fa3440647048268f9da6a8fb8f87c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5bc24dd514e390d4f7e7db41d95e777",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7c8d122799c1188276d6",
+    "url": "/portfolio/static/css/main.2664f36e.chunk.css"
+  },
+  {
+    "revision": "1335a7cfd3e3ad4809db",
+    "url": "/portfolio/static/js/2.6724e4d4.chunk.js"
+  },
+  {
+    "revision": "7c8d122799c1188276d6",
+    "url": "/portfolio/static/js/main.a9a5c59c.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "e5e01475c82f31b7f7a2bd6c486488e7",
+    "url": "/portfolio/static/media/barking-board.e5e01475.png"
+  },
+  {
+    "revision": "99969086d2a7b1ba915afd3563868e9a",
+    "url": "/portfolio/static/media/contact-information.99969086.png"
+  },
+  {
+    "revision": "7b456186878869ed02d72f720fb5bb57",
+    "url": "/portfolio/static/media/simon-dice.7b456186.png"
+  }
+]);
--- src/Canvas.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Canvas = (props) => {
-    return (<canvas id="canvas" className="canvas" width={props.frame.width} height={props.frame.height}></canvas>
-    )
-}
-
-
-export default Canvas
