c933e0d543d9ec2e9e42e78cd14aec3a1cdb2dce
ClassicalJo <jb.barrientos@yahoo.com>
2019-12-02 14:14:34 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,7 @@ import App from './App';
 import ReactGA from 'react-ga';
 import * as serviceWorker from './serviceWorker';
 
+ReactGA.initialize('UA-153521299-1');
+ReactGA.pageview('/homepage')
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
-
-function initializeReactGA() {
-    ReactGA.initialize('UA-153521299-1');
-    ReactGA.pageview('/homepage')
-}
