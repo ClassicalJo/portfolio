12e1f611808441e68a6615e45a3f196da56f40d2
Joe <jb.barrientos@yahoo.com>
2022-10-30 07:51:21 -0300

    Feat: working background animation

---
+++ src/background/animation/utils.ts
@@ -0,0 +1,61 @@
+export function resizeCanvasToDisplaySize(canvas: HTMLCanvasElement) {
+  // Lookup the size the browser is displaying the canvas in CSS pixels.
+  const displayWidth = canvas.clientWidth
+  const displayHeight = canvas.clientHeight
+
+  // Check if the canvas is not the same size.
+  const needResize = canvas.width !== displayWidth || canvas.height !== displayHeight
+
+  if (needResize) {
+    // Make the canvas the same size
+    canvas.width = displayWidth
+    canvas.height = displayHeight
+  }
+
+  return needResize
+}
+
+export function getCursorPosition(canvas: HTMLCanvasElement, evt: MouseEvent) {
+  const rect = canvas.getBoundingClientRect(), // abs. size of element
+    scaleX = canvas.width / rect.width, // relationship bitmap vs. element for x
+    scaleY = canvas.height / rect.height // relationship bitmap vs. element for y
+
+  return {
+    x: (evt.clientX - rect.left) * scaleX, // scale mouse coordinates after they have
+    y: (evt.clientY - rect.top) * scaleY // been adjusted to be relative to element
+  }
+}
+
+export function createShader(gl: WebGLRenderingContext, type: number, source: string) {
+  const shader = gl.createShader(type)
+  if (!shader) return
+  gl.shaderSource(shader, source)
+  gl.compileShader(shader)
+  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS) as boolean
+  if (success) {
+    return shader
+  }
+
+  console.log(gl.getShaderInfoLog(shader))
+  gl.deleteShader(shader)
+}
+
+export function createProgram(
+  gl: WebGLRenderingContext,
+  vertexShader: WebGLShader,
+  fragmentShader: WebGLShader
+) {
+  const program = gl.createProgram()
+  if (!program) return
+  gl.attachShader(program, vertexShader)
+  gl.attachShader(program, fragmentShader)
+  gl.linkProgram(program)
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS) as boolean
+
+  if (success) {
+    return program
+  }
+
+  console.log(gl.getProgramInfoLog(program))
+  gl.deleteProgram(program)
+}
