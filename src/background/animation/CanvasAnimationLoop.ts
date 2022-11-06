f46f4a8a443b3edb5895492dbc743a39504e40dd
Joe <jb.barrientos@yahoo.com>
2022-11-06 13:56:19 -0300

    Implements animation progress

--- src/background/animation/CanvasAnimationLoop.ts
+++ src/background/animation/CanvasAnimationLoop.ts
@@ -1,28 +1,47 @@
-import type { CanvasAnimation } from './types'
+import type { AnimationOptions, AnimationOptionsUI, CanvasAnimation } from './types'
+import { easeOutBack } from './utils'
+
+const defaultOptions: AnimationOptions = {
+  duration: 1000,
+  color: 3
+}
+
 export class CanvasAnimationLoop {
-  canvas: HTMLCanvasElement
-  animation: CanvasAnimation
   fps: number
   frameDuration: number
   time: number
   lag: number
   animationFrame: number
+  progress: number
+  options: AnimationOptions
   private _pause: boolean
-  constructor(canvas: HTMLCanvasElement, animation: CanvasAnimation) {
-    this.canvas = canvas
-    this.animation = animation
+  constructor(
+    public canvas?: HTMLCanvasElement,
+    public animation?: CanvasAnimation,
+    options?: AnimationOptionsUI
+  ) {
     this.fps = 60
     this.frameDuration = 1000 / this.fps
-    this.time = Date.now()
+    this.options = { ...defaultOptions, ...options }
+    this.time = performance.now()
     this.lag = 0
     this.animationFrame = 0
     this._pause = false
+    this.progress = 0
+  }
+
+  setCanvas(canvas: HTMLCanvasElement) {
+    this.canvas = canvas
+  }
+  setAnimation(animation: CanvasAnimation) {
+    this.animation = animation
   }
 
   loop() {
+    if (!this.canvas || !this.animation) return
     this.animationFrame = requestAnimationFrame(() => this.loop())
     //Calculate the time that has elapsed since the last frame
-    const CURRENT = Date.now()
+    const CURRENT = performance.now()
     const ELAPSED = CURRENT - this.time
     //Add the elapsed time to the lag counter
     this.lag += ELAPSED
@@ -30,13 +49,19 @@ export class CanvasAnimationLoop {
     //Update the frame if the lag counter is greater than or equal to the frame duration
     while (this.lag >= this.frameDuration) {
       //If animation is not paused update the logic
-      !this._pause && this.animation.update()
+
+      this.progress += 1 / this.options.duration
+      !this._pause &&
+        this.animation.update(easeOutBack(this.progress), this.options.color)
+
       //Reduce the lag counter by the frame duration
       this.lag -= this.frameDuration
     }
     //Calculate the lag offset and use it to render the sprites
     const lagOffset = this.lag / this.frameDuration
     this.animation.render(lagOffset)
+
+    if (this.progress >= 1) this.stop()
   }
 
   set pause(bool: boolean) {
@@ -46,11 +71,14 @@ export class CanvasAnimationLoop {
     return this._pause
   }
 
-  play() {
+  play(options: AnimationOptionsUI) {
+    this.options = { ...this.options, ...options }
+    this.time = performance.now()
     this.pause = false
     this.loop()
   }
   stop() {
+    this.progress = 0
     cancelAnimationFrame(this.animationFrame)
   }
 }
