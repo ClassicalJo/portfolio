d099161020ce38b290bcaa0f16f51a10f642466b
Joe <jb.barrientos@yahoo.com>
2022-11-17 15:54:31 -0300

    Restyling Home component with a glass card like effect

--- src/hero/Pretitle.svelte
+++ src/hero/Pretitle.svelte
@@ -1,10 +1,29 @@
 <script lang="ts"></script>
 
-<h4>Jose Barrientos</h4>
+<div>
+  <h4>Fullstack developer</h4>
+</div>
 
 <style>
+  div {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+  }
   h4 {
     font-size: 24px;
+    vertical-align: bottom;
+    font-family: 'Share Tech Mono', monospace;
+
     margin: 0px;
+    font-weight: 400;
+    font-size: 32px;
+    background: rgb(255, 255, 255);
+
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    filter: drop-shadow(3px 3px #333);
   }
 </style>
--- src/hero/Subtext.svelte
+++ src/hero/Subtext.svelte
@@ -1,6 +1,6 @@
 <script lang="ts"></script>
 
-<p>Hey there! My name is Joe, and I'm a developer from Argentina, nice to meet you!</p>
+<p>Developer</p>
 
 <style>
   p {
