02d004c00cb4f61f19c6abd7ff46aed88feb30a7
classicalJo <jb.barrientos@yahoo.com>
2023-08-20 18:31:04 -0300

    Removes assets from public folder, moved into assets inside src folder

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,13 @@
+import company from './company.svg'
+import letter from './letter.svg'
+import person from './person.svg'
+import github from './github.svg'
+import linkedin from './linkedin.svg'
+import mail from './mail.svg'
+import deploy from './deploy.svg'
+import close from './close.svg'
+import send from './send.svg'
+
+const icons = { company, letter, person, github, linkedin, mail, deploy, close, send }
+
+export default icons
---
+++ src/assets/images/index.ts
@@ -0,0 +1,10 @@
+import clip from './clip.png'
+import cloud from './cloud.png'
+import cloud2 from './cloud2.png'
+import denim from './denim.webp'
+import github from './github.png'
+import joe from './joe.jpg'
+import prism from './prism.png'
+import wave from './wave.svg'
+
+export default { clip, cloud, cloud2, denim, github, joe, prism, wave }
