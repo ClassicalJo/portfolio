cc626097483a26df7b68f51f05d590f278798fff
classicalJo <jb.barrientos@yahoo.com>
2023-09-17 05:31:30 -0300

    Adds a list of projects to the project section

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -6,5 +6,22 @@ import github from './github.png'
 import joe from './joe.jpg'
 import prism from './prism.png'
 import wave from './wave.svg'
+import aerolab from './aerolab.webp'
+import pathfinder from './pathfinder.png'
+import reflections from './reflections.png'
+import blinker from './blinker.png'
 
-export default { clip, cloud, cloud2, denim, github, joe, prism, wave }
+export default {
+  clip,
+  cloud,
+  cloud2,
+  denim,
+  github,
+  joe,
+  prism,
+  wave,
+  aerolab,
+  pathfinder,
+  reflections,
+  blinker
+}
