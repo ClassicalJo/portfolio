32553b7322449c5659564276c8b0cf54387f8e12
classicalJo <jb.barrientos@yahoo.com>
2024-10-08 20:08:28 -0300

    Updates dependencies on project

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import sveltePreprocess from 'svelte-preprocess'
+import { sveltePreprocess } from 'svelte-preprocess'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern'
+      }
+    }
+  },
   plugins: [
     svelte({
-      preprocess: [sveltePreprocess()]
+      preprocess: [sveltePreprocess()],
     })
   ]
 })
